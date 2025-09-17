8b051100d8779d8c0e425a1e464f87747bd5b014
EricSemCDev <165967197+EricSemCDev@users.noreply.github.com>
2025-09-17 00:15:13 -0300

    Corrige case do componente FundoSalada

---
+++ Frontend/src/Components/Geral/FundoSalada.jsx
@@ -0,0 +1,10 @@
+
+
+export default function FundoSalada() {
+  return (
+   <section className="bg-black w-1/2 h-screen">
+        <img src="./src/assets/salada-com-ovo.jpeg" alt="IMAGEM FUNDO"  className="w-full h-full object-cover"/>
+        <div className="absolute top-0 left-0 w-1/2 h-full bg-black opacity-20 blur-xl"></div>
+   </section>
+  );
+}
