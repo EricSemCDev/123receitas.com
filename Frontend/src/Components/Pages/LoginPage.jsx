1f8cae80ff757cc5a86d50e79e5d47f3fc96e9d6
EricSemCDev <165967197+EricSemCDev@users.noreply.github.com>
2025-09-17 00:06:05 -0300

    Update LoginPage.jsx

--- Frontend/src/Components/Pages/LoginPage.jsx
+++ Frontend/src/Components/Pages/LoginPage.jsx
@@ -1,8 +1,8 @@
 // src/Components/HomePage.jsx
-import FundoSalada from "../Geral/fundoSalada";
+import FundoSalada from "../Geral/FundoSalada";
 import Login from "../LoginPageComponents/Login";
 
-export default function CadastroPage() {
+export default function LoginPage() {
   return (
     <div className="flex">
       <FundoSalada />
